54866c8e4d56e75d31a07ef9936957eb0cdcb86f
neo <neo@neos-MacBook-Air.local>
2024-01-17 17:10:25 +0800

    update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,6 +69,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
+
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
