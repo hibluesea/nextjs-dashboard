7249f1a5c63e4e6732667e72181238f74d607e33
neo <neo@neos-MacBook-Air.local>
2024-02-02 20:50:33 +0800

    update

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -9,8 +9,9 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
+import { createInvoice } from '@/app/lib/actions';
+
 
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,15 +1,18 @@
 'use client';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
+
+
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
+import { updateInvoice } from "@/app/lib/actions";
 
 
 export default function EditInvoiceForm({ invoice, customers }: {
